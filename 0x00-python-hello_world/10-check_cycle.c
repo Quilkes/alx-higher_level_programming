b36486a0b8a82a85c35321f3bb179c120aa87037
banniroy <matinadekola33@gmail.com>
2022-09-19 14:52:26 +0100

    message

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include lists.h
+
+/**
+ * print_listint - prints all elements of a listint_t list
+ * @h: pointer to head of list
+ * Return: number of nodes
+ */
+
+
