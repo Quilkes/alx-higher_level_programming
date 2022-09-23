cf37aadd8ee8b168e77ed85913c35f7e24a325a5
Banniroy <107342239+Banniroy@users.noreply.github.com>
2022-09-23 07:56:20 +0100

    Delete 3-infinite_add.py

--- 0x02-python-import_modules/3-infinite_add.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-from sys import argv
-if __name__ == "__main__":
-    total = 0
-    argc = len(argv) - 1
-    for i in range(1, argc + 1):
-        total = total + int(argv[i])
-    print("{:d}".format(total))
