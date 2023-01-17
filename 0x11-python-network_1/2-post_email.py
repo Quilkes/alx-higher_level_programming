91d4f2a45d11801d0427858970b64fc1bd28850b
Ajibade Abdulmatin Adekola <107342239+Banniroy@users.noreply.github.com>
2023-01-17 09:12:52 +0100

    Update 2-post_email.py

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,14 +1,19 @@
 #!/usr/bin/python3
 """
-takes in a URL and an email, sends a POST request to the passed URL with the
-email as a parameter, and displays the body of the response
+Write a Python script that takes in a URL and an email, sends a POST request to the passed URL with the email as a parameter, and displays the body of the response (decoded in utf-8)
+
+The email must be sent in the email variable
+You must use the packages urllib and sys
+You are not allowed to import packages other than urllib and sys
+You don’t need to check arguments passed to the script (number or type)
+You must use the with statement
 """
 if __name__ == "__main__":
     import urllib.parse as parse
     import urllib.request as request
     from sys import argv
-    values = {'email': argv[2]}
-    data = parse.urlencode(values).encode('utf-8')
-    req = request.Request(argv[1], data)
-    with request.urlopen(req) as r:
-        print(r.read().decode('utf-8'))
+    param = {'email': argv[2]}
+    data = parse.urlencode(param).encode('utf-8')
+    doc = request.Request(argv[1], data)
+    with request.urlopen(doc) as docum:
+        print(docum.read().decode('utf-8'))
