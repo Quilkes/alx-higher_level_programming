58b9f532367536c69e9a59417a743f4f0e9b7361
Ajibade Abdulmatin Adekola <107342239+Banniroy@users.noreply.github.com>
2023-01-17 09:25:10 +0100

    Update 8-json_api.py

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,7 +1,15 @@
 #!/usr/bin/python3
 """
-takes in a letter and sends a POST request to http://0.0.0.0:5000/search_user
-with the letter as a parameter
+Write a Python script that takes in a letter and sends a POST request to http://0.0.0.0:5000/search_user with the letter as a parameter.
+
+The letter must be sent in the variable q
+If no argument is given, set q=""
+If the response body is properly JSON formatted and not empty, display the id and name like this: [<id>] <name>
+Otherwise:
+Display Not a valid JSON if the JSON is invalid
+Display No result if the JSON is empty
+You must use the package requests and sys
+You are not allowed to import packages other than requests and sys
 """
 if __name__ == '__main__':
     import requests
@@ -10,9 +18,9 @@ if __name__ == '__main__':
         q = argv[1]
     else:
         q = ""
-    r = requests.post('http://0.0.0.0:5000/search_user', data={'q': q})
+    doc = requests.post('http://0.0.0.0:5000/search_user', data={'q': q})
     try:
-        r_dict = r.json()
+        r_dict = doc.json()
         id = r_dict.get('id')
         name = r_dict.get('name')
         if len(r_dict) == 0 or not id or not name:
