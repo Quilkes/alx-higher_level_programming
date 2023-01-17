2decaeda05c669dfa1dc350a48af84fee061739c
Ajibade Abdulmatin Adekola <107342239+Banniroy@users.noreply.github.com>
2023-01-17 09:19:55 +0100

    Update 6-post_email.py

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python3
 """
-takes in a URL and an email address, sends a POST request to the passed URL
-with the email as a parameter, and finally displays the body of the response
+Write a Python script that takes in a URL and an email address, sends a POST request to the passed URL with the email as a parameter, and finally displays the body of the response.
+
+The email must be sent in the variable email
+You must use the packages requests and sys
+You are not allowed to import packages other than requests and sys
+You don’t need to error check arguments passed to the script (number or type)
 """
 if __name__ == '__main__':
     import requests
     from sys import argv
-    payload = {'email': argv[2]}
-    r = requests.post(argv[1], data=payload)
-    print(r.text)
+    payloads = {'email': argv[2]}
+    doc = requests.post(argv[1], data=payloads)
+    print(doc.text)
