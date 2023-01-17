606d065d97871d6a117b2cb1a976e0f4210f4ae1
Ajibade Abdulmatin Adekola <107342239+Banniroy@users.noreply.github.com>
2023-01-17 09:17:41 +0100

    Update 5-hbtn_header.py

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
 """
-takes in a URL, sends a request to the URL and displays the value of the
-variable X-Request-Id in the response header
+Write a Python script that takes in a URL, sends a request to the URL and displays the value of the variable X-Request-Id in the response header
+
+You must use the packages requests and sys
+You are not allow to import other packages than requests and sys
+The value of this variable is different for each request
+You don’t need to check script arguments (number and type)
 """
 if __name__ == '__main__':
     import requests
     from sys import argv
-    r = requests.get(argv[1])
-    print(r.headers.get('X-Request-Id'))
+    doc = requests.get(argv[1])
+    print(doc.headers.get('X-Request-Id'))
