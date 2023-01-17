6a22ffd3b48eb4b032bec5d1d805ac92e2bfb886
Ajibade Abdulmatin Adekola <107342239+Banniroy@users.noreply.github.com>
2023-01-17 09:31:14 +0100

    Update 100-github_commits.py

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,14 +1,20 @@
 #!/usr/bin/python3
 """
-list 10 commits (from the most recent to oldest) of the repository and user
-sent in as arguments
+Write a Python script that takes your GitHub credentials (username and password) and uses the GitHub API to display your id
+
+You must use Basic Authentication with a personal access token as password to access to your information (only read:user permission is needed)
+The first argument will be your username
+The second argument will be your password (in your case, a personal access token as password)
+You must use the package requests and sys
+You are not allowed to import packages other than requests and sys
+You don’t need to check arguments passed to the script (number or type)
 """
 if __name__ == '__main__':
     import requests
     from sys import argv
-    r = requests.get('https://api.github.com/repos/{}/{}/commits'
+    doc = requests.get('https://api.github.com/repos/{}/{}/commits'
                      .format(argv[2], argv[1]))
-    commits = r.json()
+    commits = doc.json()
     for commit in commits[:10]:
         print(commit.get('sha'), end=': ')
         print(commit.get('commit').get('author').get('name'))
